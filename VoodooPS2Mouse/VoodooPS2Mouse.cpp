4b250f2b30d5a482503964fa4f170ced6938794c
RehabMan <racerrehabman@gmail.com>
2013-08-05 11:37:45 -0700

    bump version 1.8.6

--- VoodooPS2Mouse/VoodooPS2Mouse.cpp
+++ VoodooPS2Mouse/VoodooPS2Mouse.cpp
@@ -123,7 +123,7 @@ bool ApplePS2Mouse::init(OSDictionary * dict)
     removeProperty("TrackpadScroll");
   }
 
-  IOLog("VoodooPS2Mouse Version 1.8.5 loaded...\n");
+  IOLog("VoodooPS2Mouse Version 1.8.6 loaded...\n");
 	
   return true;
 }
