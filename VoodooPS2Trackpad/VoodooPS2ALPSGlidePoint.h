52767bd27999105b0e27de862dfdc78cdc3fb6a2
SkyrilHD <skyrilhd.info@gmail.com>
2022-05-25 16:15:54 +0200

    remove obsolete code

--- VoodooPS2Trackpad/VoodooPS2ALPSGlidePoint.h
+++ VoodooPS2Trackpad/VoodooPS2ALPSGlidePoint.h
@@ -47,134 +47,6 @@
 #define DOLPHIN_PROFILE_XOFFSET		8	/* x-electrode offset */
 #define DOLPHIN_PROFILE_YOFFSET		1	/* y-electrode offset */
 
-//
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// SimpleAverage Class Declaration
-//
-
-template <class T, int N>
-class SimpleAverage
-{
-private:
-    T m_buffer[N];
-    int m_count;
-    int m_sum;
-    int m_index;
-
-public:
-    inline SimpleAverage() { reset(); }
-    T filter(T data)
-    {
-        // add new entry to sum
-        m_sum += data;
-        // if full buffer, then we are overwriting, so subtract old from sum
-        if (m_count == N)
-            m_sum -= m_buffer[m_index];
-        // new entry into buffer
-        m_buffer[m_index] = data;
-        // move index to next position with wrap around
-        if (++m_index >= N)
-            m_index = 0;
-        // keep count moving until buffer is full
-        if (m_count < N)
-            ++m_count;
-        // return average of current items
-        return m_sum / m_count;
-    }
-    inline void reset()
-    {
-        m_count = 0;
-        m_sum = 0;
-        m_index = 0;
-    }
-    inline int count() const { return m_count; }
-    inline int sum() const { return m_sum; }
-    T oldest() const
-    {
-        // undefined if nothing in here, return zero
-        if (m_count == 0)
-            return 0;
-        // if it is not full, oldest is at index 0
-        // if full, it is right where the next one goes
-        if (m_count < N)
-            return m_buffer[0];
-        else
-            return m_buffer[m_index];
-    }
-    T newest() const
-    {
-        // undefined if nothing in here, return zero
-        if (m_count == 0)
-            return 0;
-        // newest is index - 1, with wrap
-        int index = m_index;
-        if (--index < 0)
-            index = m_count-1;
-        return m_buffer[index];
-    }
-    T average() const
-    {
-        if (m_count == 0)
-            return 0;
-        return m_sum / m_count;
-    }
-};
-
-//
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// DecayingAverage Class Declaration
-//
-
-template <class T, class TT, int N1, int N2, int D>
-class DecayingAverage
-{
-private:
-    T m_last;
-    bool m_lastvalid;
-
-public:
-    inline DecayingAverage() { reset(); }
-    T filter(T data, int fingers)
-    {
-        TT result = data;
-        TT last = m_last;
-        if (m_lastvalid)
-            result = (result * N1) / D + (last * N2) / D;
-        m_lastvalid = true;
-        m_last = (T)result;
-        return m_last;
-    }
-    inline void reset()
-    {
-        m_lastvalid = false;
-    }
-};
-
-template <class T, class TT, int N1, int N2, int D>
-class UndecayAverage
-{
-private:
-    T m_last;
-    bool m_lastvalid;
-
-public:
-    inline UndecayAverage() { reset(); }
-    T filter(T data)
-    {
-        TT result = data;
-        TT last = m_last;
-        if (m_lastvalid)
-            result = (result * D) / N1 - (last * N2) / N1;
-        m_lastvalid = true;
-        m_last = (T)data;
-        return (T)result;
-    }
-    inline void reset()
-    {
-        m_lastvalid = false;
-    }
-};
-
 struct alps_hw_state {
     int x;
     int y;
@@ -182,9 +54,7 @@ struct alps_hw_state {
     int virtualFingerIndex;
 };
 
-struct synaptics_virtual_finger_state {
-    SimpleAverage<int, 5> x_avg;
-    SimpleAverage<int, 5> y_avg;
+struct alps_virtual_finger_state {
     UInt32 x;
     UInt32 y;
     uint8_t pressure;
@@ -528,21 +398,16 @@ private:
     uint32_t physical_max_y {0};
 
     alps_hw_state fingerStates[MAX_TOUCHES] {};
-    synaptics_virtual_finger_state virtualFingerStates[MAX_TOUCHES] {};
+    alps_virtual_finger_state virtualFingerStates[MAX_TOUCHES] {};
     bool freeFingerTypes[kMT2FingerTypeCount];
 
     static_assert(MAX_TOUCHES <= kMT2FingerTypeLittleFinger, "Too many fingers for one hand");
 
     int clampedFingerCount {0};
-    bool wasSkipped {false};
     
     bool reportVoodooInput;
 
     int minXOverride {-1}, minYOverride {-1}, maxXOverride {-1}, maxYOverride {-1};
-
-    int lastFingerCount;
-    int lastSentFingerCount;
-    bool hadLiftFinger;
 
     ForceTouchMode _forceTouchMode {FORCE_TOUCH_DISABLED};
     int _forceTouchPressureThreshold {100};
@@ -663,16 +528,6 @@ private:
     void set_resolution();
     void alps_buttons(struct alps_fields &f);
 
-    int dist(int physicalFinger, int virtualFinger);
-    void assignVirtualFinger(int physicalFinger);
-    void assignFingerType(synaptics_virtual_finger_state &vf);
-    void freeAndMarkVirtualFingers();
-    int upperFingerIndex() const;
-    const alps_hw_state& upperFinger() const;
-    void swapFingers(int dst, int src);
-    /// Translates physical fingers into virtual fingers so that host software doesn't see 'jumps' and has coordinates for all fingers.
-    /// @return True if is ready to send finger state to host interface
-    bool renumberFingers();
     void sendTouchData();
     static MT2FingerType GetBestFingerType(int i);
 
