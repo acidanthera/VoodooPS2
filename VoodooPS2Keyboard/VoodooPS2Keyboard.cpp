2f0d204cd1cda1a9925b722ae7f7126cdaab2fae
Die Peter Pan <diepeterpan@gmail.com>
2022-09-29 11:46:51 +0200

    Replace kDP_SetDefaultsAndDisable with kDP_SetDefaults to fix keyboard disabled in some UEFI (#53)

--- VoodooPS2Keyboard/VoodooPS2Keyboard.cpp
+++ VoodooPS2Keyboard/VoodooPS2Keyboard.cpp
@@ -1923,7 +1923,7 @@ void ApplePS2Keyboard::setKeyboardEnable(bool enable)
     // (keyboard enable/disable command)
     TPS2Request<2> request;
     request.commands[0].command = kPS2C_WriteDataPort;
-    request.commands[0].inOrOut = enable ? kDP_Enable : kDP_SetDefaultsAndDisable;
+    request.commands[0].inOrOut = enable ? kDP_Enable : kDP_SetDefaults;
     request.commands[1].command = kPS2C_ReadDataPortAndCompare;
     request.commands[1].inOrOut = kSC_Acknowledge;
     request.commandsCount = 2;
